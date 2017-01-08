51ceaad493ac10d17db693d57d883ab4e234e0c7
Tiago Martinho <tiago@elit.software>
2017-01-08 12:09:22 +0100

    Initial Commit

---
+++ SourcerySpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SourcerySpike
+//
+//  Created by Tiago Martinho on 08/01/2017.
+//  Copyright © 2017 Tiago Martinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
